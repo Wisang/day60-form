5a263c9ddb7fa85f9d2a7359a8a29d0380e7f79f
wisang <wseom7@gmail.com>
2022-08-06 23:26:08 +0900

    contact.html added and basic interaction works

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 @app.route('/login', methods=['post'])
 def receive_data():
     username = request.form["username"]
     password = request.form["password"]
     return render_template("login.html", username=username, password=password)
+
+
+@app.route('/contact-info', methods=['post'])
+def contact_info():
+    # username = request.form["username"]
+    # password = request.form["password"]
+    return "<h1>Data Transfer Successful</h1>"
 
 
 if __name__ == "__main__":
